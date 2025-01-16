f817dbf62463cd1b00baf9d656662067feb8fd49
joaosantosmedeiros <jopesame@gmail.com>
2025-01-16 13:58:34 -0300

    receita 13

---
+++ receita13/app/teste/page.js
@@ -0,0 +1,13 @@
+import { Componente2 } from "../page";
+
+export default function Teste() {
+    return (
+        <div>
+            <div>Apenas um teste</div>
+            <div>
+                <h1>Adeus, mundo!</h1>
+                <Componente2/>
+            </div>
+        </div>
+    )
+}
---
+++ receita13/app/youtube/page.js
@@ -0,0 +1,13 @@
+import { Componente1 } from "../page"
+
+export default function Youtube() {
+    return (
+        <div>
+            <div>Youtube</div>
+            <div>
+                <h1>bem vindos ao meu canal!</h1>
+            </div>
+            <Componente1 />
+        </div>
+    )
+}
